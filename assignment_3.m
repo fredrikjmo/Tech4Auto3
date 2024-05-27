1d9b385931d669978ee378d1137019243578fb8b
Fredrik Johan Stokke Mo <fjmo@stud.ntnu.no>
2024-05-27 11:36:46 +0200

    minor changeups

--- assignment_3.m
+++ assignment_3.m
@@ -8,6 +8,8 @@ l_r = 1.5;
 l_f = 1.3;
 J_psi = 2875;
 
+delta_constraint = deg2rad(25);
+
 V_x = 20; % [m/s]
 
 %% State space model definition
@@ -20,3 +22,11 @@ A = [0                1                             0                           
 B_1 = [0; C_f/m_v; 0; (C_f*l_f)/J_psi];
 
 B_2 = [0; (C_r*l_r - C_f*l_f)/(m_v*V_x) - V_x; 0; -(C_r*l_r^2 + C_f*l_f^2)/(J_psi*V_x)];
+
+C = eye(4);
+
+%% Feedback and feedforward control matrices
+
+K = [1 1 1 1];
+
+K_ff = 1;
